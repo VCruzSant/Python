95db92fa71e8318dcd51e9710e7b161d185d90f3
VCruzSant <sviniantiago@gmail.com>
2023-04-04 16:29:40 -0300

    learning If
    if, elif, else

---
+++ py3/conditional.py
@@ -0,0 +1,11 @@
+#if / elif..../ else
+#se/ se não se/ se não
+
+enter = input('Você deseja "entrar" ou "sair"? ')
+
+if enter == 'entrar':
+    print('você entrou no Sistema')
+elif enter == 'sair':
+    print('você saiu no Sistema')
+else:
+    print('Digite corretamente o que deseja')
