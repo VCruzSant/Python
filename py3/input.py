266b8ce3052343f0119a7a89a95a58dd5f77abcc
VCruzSant <sviniantiago@gmail.com>
2023-04-04 15:06:41 -0300

    learning Input

--- py3/input.py
+++ py3/input.py
@@ -6,9 +6,9 @@ print (f'seu nome é: {name=}') #nome da varivável com '=' mostra o nome da var
 numb1 = input('Digite um número: ')
 numb2 = input('Digite outro número: ')
 
-flonumb1 = float(numb1)
+flonumb1 = float(numb1)#coerção
 flonumb2 = float(numb2)
 
-rnumb = flonumb1 + flonumb2
+rnumb = flonumb1 + flonumb2 #soma os números ja convertidos pela coerção
 
-print(f'A soma desses números é {rnumb}')
+print(f'A soma desses números é {rnumb}')#apresenta o resultado
