5c48c6af4582318e6e501dbd362a9e91b98037c0
VCruzSant <sviniantiago@gmail.com>
2023-04-05 16:25:34 -0300

    fatiamento

---
+++ py3/fatiamento.py
@@ -0,0 +1,10 @@
+'''
+ fatiamento -> [i: f: p:] [::]
+ obs: função len retorna a qtd de caracters da string
+'''
+vari = 'Hello Word'
+print(vari[6:]) #comece a partir do 6 e vá até o fim
+print(vari[6:9]) #comece a partir do 6 e pare no 8, se eu quiser que selecione o 8, tem que ir um a +
+print(len(vari[6:9])) #conta o tanto de caracters, inclusive as que eu estou pegando 
+print(vari[0:5:2]) #de 2 em 2 
+print(vari[::-1]) #conta invertido
