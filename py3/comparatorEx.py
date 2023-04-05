3f281e2b3b09032524de8fb97ed3c6c5f3d286df
VCruzSant <sviniantiago@gmail.com>
2023-04-05 11:14:18 -0300

    exerciseComparator

---
+++ py3/comparatorEx.py
@@ -0,0 +1,9 @@
+value1 = input('Digite um valor: ')
+value2 = input('Digite outro valor: ')
+
+if value1 > value2:
+    print(f'O primeiro valor: {value1=} é maior do que o segundo valor:{value2=}')
+elif value1 == value2:
+    print('Os valores são iguais')
+else:
+    print(f'O segundo valor: {value2=} é maior do que o primeiro {value1=}') 
