90af728afe1bb963ebb2dc0d88613443c87ce8fd
VCruzSant <sviniantiago@gmail.com>
2023-04-06 18:41:18 -0300

    exercises
    exercises about all that y learned

--- py3/exEnun.py
+++ py3/exEnun.py
@@ -1,23 +1,22 @@
-'''
-number = input('Digite um número inteiro: ')
+number = input("Digite um número inteiro: ")
 
 
 if number.isdigit():
     intNumb = int(number)
-    
+
     if (intNumb % 2) == 0:
-        print('Seu número é par')
+        print("Seu número é par")
 
     else:
-        print('Seu número é impar')
+        print("Seu número é impar")
 
 else:
-    print('Digite um número valido')
+    print("Digite um número valido")
 
-print('-' * 10)
-'''
+print("-" * 10)
 
-hour = input('Digite que horas são: ')
+
+hour = input("Digite que horas são: ")
 
 try:
     hNumber = float(hour)
@@ -25,12 +24,26 @@ try:
     if hNumber >= 0 and hNumber <= 11:
         print("Bom dia")
 
-    elif hNumber >=12 and hNumber <=17:
-        print('Boa Tarde')
+    elif hNumber >= 12 and hNumber <= 17:
+        print("Boa Tarde")
 
-    elif hNumber >= 18 and hNumber <=23:
-        print('Boa noite')
+    elif hNumber >= 18 and hNumber <= 23:
+        print("Boa noite")
 
 
 except:
     print("digite uma hora valida")
+
+print('-' * 10)
+
+name = input("Digite seu nome: ")
+tname = len(name)
+
+if tname > 1 and tname <= 4:
+    print("Seu nome é curto")
+elif tname == 5:
+    print("Seu nome é normal")
+elif tname > 6:
+    print("Seu nome é gande")
+else:
+    print("não entendi seu nome")
