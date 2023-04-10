989d4e4ed7563c936cf1ac1248ae38bf60931d53
VCruzSant <sviniantiago@gmail.com>
2023-04-10 12:19:24 -0300

    prj Calculator
    Calculator with While

--- prj/whileCalculator.py
+++ prj/whileCalculator.py
@@ -1,64 +1,61 @@
-'''Calculator with While'''
+"""Calculator with While"""
 while True:
-    num_1 = input('Digite um número: ') 
-    num_2 = input('Digite outro número: ')
-    operator = input('Qual operação( + | - | / | * ): ')
+    num_1 = input("Digite um número: ")
+    num_2 = input("Digite outro número: ")
+    operator = input("Qual operação( + | - | / | * ): ")
 
-    num_valid = None #flag
+    num_valid = None  # flag
 
-
-    float_num_1 = 0 #declarando variavel fora de bloco para não gerar algum bug
+    float_num_1 = 0  # declarando variavel fora de bloco para não gerar algum bug
     float_num_2 = 0
-    
-
 
     try:
         float_num_1 = float(num_1)
         float_num_2 = float(num_2)
-        num_valid = True #verificando se foi convertido
-    
+        num_valid = True  # verificando se foi convertido
+
     except Exception as error:
-        num_valid = None #se não for convertido, de erro
+        num_valid = None  # se não for convertido, de erro
         print(error)
 
-    if num_valid is None: #se não for convertido, imprima e continue novamente
-        print('Um ou ambos os números não são validos, digite outros')
+    if num_valid is None:  # se não for convertido, imprima e continue novamente
+        print("Um ou ambos os números não são validos, digite outros")
         continue
 
+    operator_perm = "+-/*"
 
-
-    operator_perm = '+-/*'
-
-    if operator not in operator_perm: #se o operador selecionado não está entre os permitidos, imprima e continue novamente
-        print('operação invalida')
+    if (
+        operator not in operator_perm
+    ):  # se o operador selecionado não está entre os permitidos, imprima e continue novamente
+        print("operação invalida")
         continue
 
-
-    if len(operator) > 1: #verifica a quantidade de operadores
-        print('Digite apenas um operador')
+    if len(operator) > 1:  # verifica a quantidade de operadores
+        print("Digite apenas um operador")
         continue
 
+    print("Resultado:")
 
-    print('Resultado:')
-
-    if operator == '+':
+    if operator == "+":
         calc = float_num_1 + float_num_2
-        print(f'{float_num_1} + {float_num_2} = {calc}')
+        print(f"{float_num_1} + {float_num_2} = {calc}")
 
-    if operator == '-':
+    if operator == "-":
         calcsub = float_num_1 - float_num_2
-        print(f'{float_num_1} - {float_num_2} = {calcsub}')
+        print(f"{float_num_1} - {float_num_2} = {calcsub}")
 
-    if operator == '/':
+    if operator == "/":
         calcdiv = float_num_1 / float_num_2
-        print(f'{float_num_1} / {float_num_2} = {calcdiv}')
+        print(f"{float_num_1} / {float_num_2} = {calcdiv}")
 
-    if operator == '*':
+    if operator == "*":
         calcmult = float_num_1 * float_num_2
-        print(f'{float_num_1} * {float_num_2} = {calcmult}')
+        print(f"{float_num_1} * {float_num_2} = {calcmult}")
 
-    sair = input('Quer sair? [s]im: ').lower().startswith('s') #converte para minúsculo e verifica se começa com 's' -> bool
+    sair = (
+        input("Quer sair? [s]im: ").lower().startswith("s")
+    )  # converte para minúsculo e verifica se começa com 's' -> bool
 
     if sair is True:
-        print('você saiu')
-        break #para o programa se for True
+        print("você saiu")
+        break  # para o programa se for True
