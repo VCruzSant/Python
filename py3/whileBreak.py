ee5617a002bb2be40a377fc803312f5cb1870737
VCruzSant <sviniantiago@gmail.com>
2023-04-10 00:57:55 -0300

    Create whileBreak.py

---
+++ py3/whileBreak.py
@@ -0,0 +1,10 @@
+'''
+Repetições
+While --> Enquanto 
+faça uma ação, ENQUANTO isso for verdadeiro 
+'''
+condicao = True
+while condicao:
+    name = input("Qual seu nome:")
+    print(f'seu nome é: {name}')
+#enquanto minha condição for true, vai ser executado o while até a condicao ser false, vai ficar perguntando meu nome até eu mudar a condicao
