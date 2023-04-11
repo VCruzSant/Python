760a485a0f617e4892e50ba5c97683ef738861ee
VCruzSant <sviniantiago@gmail.com>
2023-04-10 23:07:50 -0300

    Iterator
    how it works the for`s Iterator

---
+++ py3/iterator.py
@@ -0,0 +1,17 @@
+'''
+Iterável -> str, range, etc (__iter__)
+Iterador -> quem sabe entregar um valor por vez
+next -> me entregue o próximo valor
+iter -> me entregue seu iterador
+
+'''
+text = 'anything'
+iterator = iter(text)
+
+while True:
+    try: 
+        letra = next(iterator)
+        print(letra)
+
+    except StopIteration:
+        break
---
+++ py3/rangeFor.py
@@ -0,0 +1,7 @@
+'''
+Range -> range(start, stop, step)
+'''
+numbers = range (0, 11, 2)
+
+for numb in numbers:
+    print(numb)
