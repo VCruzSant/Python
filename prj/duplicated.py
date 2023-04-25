da01fd41c783b3b356c6bf24bf0536713feb7acd
VCruzSant <sviniantiago@gmail.com>
2023-04-25 13:59:08 -0300

    prj dulicated
    a first duplicate number identifier

---
+++ prj/duplicated.py
@@ -0,0 +1,34 @@
+list_of_list = [
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    [9, 1, 8, 9, 9, 7, 2, 1, 6, 8],
+    [1, 3, 2, 2, 8, 6, 5, 9, 6, 7],
+    [3, 8, 2, 8, 6, 7, 7, 3, 1, 9],
+    [4, 8, 8, 8, 5, 1, 10, 3, 1, 7],
+    [1, 3, 7, 2, 2, 1, 5, 1, 9, 9],
+    [10, 2, 2, 1, 3, 5, 10, 5, 10, 1],
+    [1, 6, 1, 5, 1, 1, 1, 4, 7, 3],
+    [1, 3, 7, 1, 10, 5, 9, 2, 5, 7],
+    [4, 7, 6, 5, 2, 9, 2, 1, 2, 1],
+    [5, 3, 1, 8, 5, 7, 1, 8, 8, 7],
+    [10, 9, 8, 7, 6, 5, 4, 3, 2, 1],
+]
+
+def look_duplicated(list_int):
+    numb_checked = set()
+    first_duplicated = -1
+
+    for numb in list_int:
+        if numb in numb_checked:
+            first_duplicated = numb
+            break
+        
+        numb_checked.add(numb)
+
+    print()
+    return first_duplicated
+
+for listin in list_of_list:
+    print(
+        listin,
+        look_duplicated(listin)
+    )
