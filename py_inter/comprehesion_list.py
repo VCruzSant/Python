273e51a9db314d14d3bedae3ae1d12e4be2c3c5f
VCruzSant <sviniantiago@gmail.com>
2023-04-26 15:53:28 -0300

    Update comprehesion_list.py

--- py_inter/comprehesion_list.py
+++ py_inter/comprehesion_list.py
@@ -1,5 +1,5 @@
 #List Comprehesion -> forma rápida de criar listas
-print(list(range(10)))
+#print(list(range(10)))
 
 lista = []
 for numb in range(10):
@@ -7,10 +7,25 @@ for numb in range(10):
 # print(lista)
 
 lista_1 = [1 for numb in range(10) ]
-print(lista_1)
+# print(lista_1)
 
 lista = [numb for numb in range(10)]
-print(lista)
+# print(lista)
 
 list_mult = [numb * 2 for numb in range(10)]
-print(list_mult)
+# print(list_mult)
+
+#mapeamento de dados in list comprehension
+products = [
+    {'nome': 'p1', 'preco': 20, },
+    {'nome': 'p2', 'preco': 10, },
+    {'nome': 'p3', 'preco': 30, },
+]
+
+new_products = [
+    {**prdt, 'preco': prdt['preco'] * 1.1}
+    if prdt['preco'] > 10 else {**prdt}
+    for prdt in products
+]
+
+print(*new_products, sep='\n')
