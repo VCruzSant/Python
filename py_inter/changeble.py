2369f6c5af86b2b0a68d13acd704b07dce64c92c
VCruzSant <sviniantiago@gmail.com>
2023-04-27 10:43:23 -0300

    falsy

---
+++ py_inter/changeble.py
@@ -0,0 +1,24 @@
+lista = []
+dicionario = {}
+conjunto = set()
+tupla = ()
+string = ''
+inteiro = 0
+flutuante = 0.0
+nada = None
+falso = False
+intervalo = range(0)
+
+def falsy(valor):
+    return '= Falsy' if  not valor else '= Truthy' #if  not -> significa que o valor é falso
+
+print(f'{lista=}', falsy(lista))
+print(f'{dicionario=}', falsy(dicionario))
+print(f'{conjunto=}', falsy(conjunto))
+print(f'{tupla=}', falsy(tupla))
+print(f'{string=}', falsy(string))
+print(f'{inteiro=}', falsy(inteiro))
+print(f'{flutuante=}', falsy(flutuante))
+print(f'{nada=}', falsy(nada))
+print(f'{falso=}', falsy(falso))
+print(f'{intervalo=}', falsy(intervalo))
