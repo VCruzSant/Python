73a9f70f8a5583b797c34c9e3443ec33eb0b5c61
VCruzSant <sviniantiago@gmail.com>
2023-04-27 11:31:48 -0300

    Update generator.py

--- py_inter/generator.py
+++ py_inter/generator.py
@@ -5,8 +5,8 @@ iterator = iter(iterable) #-> __iter__ & __next__
 
 #Genetator -> pause
 
-lista = [l for l in range(100)]
-generator = (g for g in range(100))
+lista = [l for l in range(100)] #está na memória
+generator = (g for g in range(100))# generator espera pedir um valor para ele antes de colocar na memória
 
 print(sys.getsizeof(lista))
 print(sys.getsizeof(generator))
