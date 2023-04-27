0fdbc6662a7cf63d17f6411bfb1d4c3a36476303
VCruzSant <sviniantiago@gmail.com>
2023-04-27 12:54:07 -0300

    Update ex_generator.py

--- py_inter/ex_generator.py
+++ py_inter/ex_generator.py
@@ -1,12 +1,15 @@
-def generator(n=0):
+def generator(n=0, max=10):
     while True:
         print(n)
         yield n
         n += 1
         input("mais um?")
 
+        if n > max:
+            return 'finish'
 
-gene = generator(n=0)
+
+gene = generator(n=0, max =10)
 contin = input("Start? ")
 
 if contin:
