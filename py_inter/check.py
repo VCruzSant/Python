94c534ee601d734971a756c9af52538147b399c3
VCruzSant <sviniantiago@gmail.com>
2023-04-27 11:10:53 -0300

    geta and hesa

---
+++ py_inter/check.py
@@ -0,0 +1,10 @@
+# dir, hasattr, getattr
+string = 'santiago'
+metodo_up = 'upper'#atributo pode ser chamado por uma variavel, porém deve estar entre ''
+
+if hasattr(string, metodo_up) is True: #hasattr, chega se um OBJ tem um determinado atributo
+    print('existe upper')
+    print(getattr(string, metodo_up)())
+
+else:
+    print('não existe')
