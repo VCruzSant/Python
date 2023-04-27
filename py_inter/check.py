591c08481efea1b6b871771c9d1055507a2fd006
VCruzSant <sviniantiago@gmail.com>
2023-04-27 11:33:43 -0300

    checking iter

--- py_inter/check.py
+++ py_inter/check.py
@@ -2,9 +2,10 @@
 string = 'santiago'
 metodo_up = 'upper'#atributo pode ser chamado por uma variavel, porém deve estar entre ''
 
-if hasattr(string, metodo_up) is True: #hasattr, chega se um OBJ tem um determinado atributo
+if hasattr(string, metodo_up) is True: #hasattr -> chega se um OBJ tem um determinado atributo
     print('existe upper')
-    print(getattr(string, metodo_up)())
+    print(getattr(string, metodo_up)())#getattr -> pega o atributo 
 
 else:
     print('não existe')
+    
