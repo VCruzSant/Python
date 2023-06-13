0a0cc750f843ee199a1aa83cef179830391c6a57
VCruzSant <sviniantiago@gmail.com>
2023-06-13 19:03:51 -0300

    Update sobreposition.py

--- object oriented programming/sobreposition.py
+++ object oriented programming/sobreposition.py
@@ -18,16 +18,30 @@
 
 class A():
     atribut_a = 'value a'
+
+    def __init__(self, atribute):
+        self.atribute = 'atributo A'
+
     def metod(self):
         print('A')
 
 class B(A):
     atribut_b = 'value b'
+
+
+    def __init__(self, atribute, atri_b):
+        super().__init__(atribute)
+        self.atri_b = atri_b
+
     def metod(self):
         print('B')
 
 class C(B):
     atribut_c = 'value c'
+
+    def __init__(self, *args, **kwarg):
+        super().__init__(*args, **kwarg)
+
     def metod(self): 
         super(B, self).metod()
         super(C, self).metod() # -> mesma coisa que super().metod() de mandeira explicita
