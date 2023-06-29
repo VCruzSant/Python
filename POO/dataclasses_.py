609b8ae994d61a727bfda5a297e6792f943413a1
VCruzSant <sviniantiago@gmail.com>
2023-06-29 20:38:10 -0300

    Update dataclasses_.py

--- POO/dataclasses_.py
+++ POO/dataclasses_.py
@@ -6,26 +6,36 @@
 # Foi descrito na PEP 557 e adicionado na versão 3.7 do Python.
 # doc: https://docs.python.org/3/library/dataclasses.html
 
-from dataclasses import dataclass
+from dataclasses import dataclass, asdict, astuple
+
+# frozen = True -> não deixa setar nada na classe
 
 
-@dataclass
+@dataclass(frozen=True, order=True)
 class People:
     name_: str
     age: int
+
+    def __post_init__(self):
+        print('venho dps do init')
 
     @property
     def name(self) -> str:
         return self.name_
 
-    @name.setter
-    def name(self, value) -> str:
-        self.name_ = value
-        return self.name_
+    # @name.setter
+    # def name(self, value) -> str:
+    #     self.name_ = value
+    #     return self.name_
 
 
 if __name__ == '__main__':
     p1 = People('Vini', 20)
-    print(p1)
-    p1.name = 'vini mesmo'
-    print(p1)
+    # print(p1)
+    # p1.name = 'vini mesmo'
+    # print(p1)
+    # list_ = [People('B', 10), People('A', 10), People('C', 10)]
+    # ordened = sorted(list_, reverse=True, key=lambda p: p.name)
+    # print(ordened)
+    print(astuple(p1))
+    print(asdict(p1))
