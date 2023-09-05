5100fc10df7c5d44f539199031340713971c91e3
VCruzSant <sviniantiago@gmail.com>
2023-09-04 23:33:49 -0300

    Learning selenium

--- modules/automatic_nav/main.py
+++ modules/automatic_nav/main.py
@@ -1,8 +1,19 @@
 import time
 from pathlib import Path
 
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
+from selenium import webdriver  # webdriver para o ".Chrome" e "ChromeOptions"
+from selenium.webdriver.chrome.service import Service  # driver executavel
+from selenium.webdriver.common.by import By  # seleciona o obj da page
+from selenium.webdriver.common.keys import Keys  # tecla importante ex: enteder
+# Codição esperada:
+from selenium.webdriver.support import expected_conditions as EC
+# faz a espera de algo ser exibido para continuar a ação:
+from selenium.webdriver.support.wait import WebDriverWait
+
+# Chrome Options
+# https://peter.sh/experiments/chromium-command-line-switches/
+# Doc Selenium
+# https://selenium-python.readthedocs.io/locating-elements.html
 
 
 ROOT_FOLDER = Path(__file__).parent
@@ -31,9 +42,34 @@ def make_chrome_browser(*options: str) -> webdriver.Chrome:
 
 
 if __name__ == '__main__':
+    TIME_TO_WAIT = 10
     options = ('--disable-gpu', '--no-sandbox',)
 
-    # abrir um site:
+    # atribuindo a função que configura no navegador
     browser = make_chrome_browser(*options)
+    # navegador abra nesse site:
     browser.get('https://www.google.com.br/')
-    time.sleep(30)
+
+    # navegador espere para encontrar o input, tempo de espera, espere até que:
+    search_input = WebDriverWait(browser, TIME_TO_WAIT).until(
+        # a condição de espera é a preseça do elemento ser localizada:
+        EC.presence_of_element_located(
+            # pelo nome 'q' -> name html
+            (By.NAME, 'q')
+        )
+    )
+    # pesquise por:
+    search_input.send_keys('Hello World!')
+    # de enteder:
+    search_input.send_keys(Keys.ENTER)
+
+    # Caso tenha certeza que o elemento vai estar na tela, pode pular o Wait:
+    # Selecionando os resultados:
+    results = browser.find_element(By.ID, 'search')
+    # Dos resultados, selecione os links
+    links = results.find_elements(By.TAG_NAME, 'a')
+    # clique no primeiro link:
+    links[0].click()
+
+    # dorme por 10 segundos:
+    time.sleep(TIME_TO_WAIT)
