59ba34681ae2bb933e654cb10a7f61a7532a8d4e
VCruzSant <sviniantiago@gmail.com>
2023-09-25 21:00:55 -0300

    Leaning signals and slots

--- PySide6_/mainWindow.py
+++ PySide6_/mainWindow.py
@@ -10,6 +10,7 @@
 # -> exec
 
 import sys
+from PySide6.QtCore import Slot
 from PySide6.QtWidgets import (
     QApplication, QPushButton, QWidget, QGridLayout, QMainWindow)
 
@@ -52,19 +53,54 @@ layout.addWidget(button3, 2, 1, 1, 2)
 status_bar = window.statusBar()
 status_bar.showMessage('a message')
 
+# Slots -> Funções que se conectam a eventos:
 
+
+@Slot()
 def slot_example(status_bar):
     status_bar.showMessage('Show this Message')
+
+
+@Slot()
+def other_slot(checked):
+    print('is checked?', checked)
+
+# adiando a execução do slot:
+# o signal hovered não possui argumentos a serem passados
+# então a função interna verifica se está checkado, passando assim o True/False
+# correspondente
+
+
+# def thirdy_slot(action):
+#     def inner():
+#         other_slot(action.isChecked())
+#     return inner
+
+# mesma coisa com lambda:
+@Slot()
+def thirdy_slot(action):
+    return other_slot(action.isChecked())
 
 
 # menu bar
 menu_bar = window.menuBar()
 first_menu = menu_bar.addMenu('first Menu')
-first_action = first_menu.addAction('First action')
 
+# Signals -> Eventos:
+
+first_action = first_menu.addAction('First action')
 # trigger -> quando é clicado
 # quando a minha ação for clicada, execute a função slot
 first_action.triggered.connect(lambda: slot_example(status_bar))
+
+second_action = first_menu.addAction('Second action')
+# quando a minha ação for clicada, check
+second_action.setCheckable(True)
+second_action.toggled.connect(other_slot)  # Me conectando a um slot
+# quando meu mouse estiver em cima:
+second_action.hovered.connect(lambda: thirdy_slot(second_action))
+
+button.clicked.connect(lambda: thirdy_slot(second_action))
 
 window.show()
 app.exec()  # loop da aplicação
