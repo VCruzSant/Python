d18ff2e1c54fee17441850136e16f1ad89fdc15d
VCruzSant <sviniantiago@gmail.com>
2023-09-25 22:34:40 -0300

    migration for classes

--- PySide6_/class_PySide.py
+++ PySide6_/class_PySide.py
@@ -3,9 +3,6 @@ from PySide6.QtCore import Slot
 from PySide6.QtWidgets import (
     QApplication, QPushButton, QWidget, QGridLayout, QMainWindow)
 
-# a vida da minha tela:
-app = QApplication(sys.argv)
-
 # definindo window principal
 
 
@@ -13,87 +10,79 @@ class MyWindow(QMainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
 
+        # widget principal:
+        self.central_widget = QWidget()
+        # definindo window principal com widget principal
+        self.setCentralWidget(self.central_widget)
+        # titulo para window:
+        self.setWindowTitle('My Window Python')
 
-window = MyWindow()
-# widget principal:
-central_widget = QWidget()
-# definindo window principal com widget principal
-window.setCentralWidget(central_widget)
-# titulo para window:
-window.setWindowTitle('My Window Python')
+        # buttons
+        self.button = self.make_button('B1')
+        self.button.clicked.connect(self.second_slot_check)
 
-button2 = QPushButton('My Button 2')
-button2.setStyleSheet('font-size: 35px; color: black')
+        self.button2 = self.make_button('B2')
+        self.button2.setStyleSheet('font-size: 35px; color: black')
 
-button3 = QPushButton('My Button 3')
-button3.setStyleSheet('font-size: 35px; color: black')
-# button.show()  # adiciona o widget na hierarquia e exibe na tela]
+        self.button3 = self.make_button('B3')
+        self.button3.setStyleSheet('font-size: 35px; color: black')
+        # button.show()  # adiciona o widget na hierarquia e exibe na tela
 
-# layout onde coloco todos meus outros widgets
-layout = QGridLayout()
-# apontando meu layout para widget principal
-central_widget.setLayout(layout)
-# apontando botão para meu layout
-layout.addWidget(button, 1, 1)
-layout.addWidget(button2, 1, 2)
-layout.addWidget(button3, 2, 1, 1, 2)
+        # layout onde coloco todos meus outros widgets
+        self.grid_layout = QGridLayout()
+        # apontando meu layout para widget principal
+        self.central_widget.setLayout(self.grid_layout)
+        # apontando botão para meu layout
+        self.grid_layout.addWidget(self.button, 1, 1)
+        self.grid_layout.addWidget(self.button2, 1, 2)
+        self.grid_layout.addWidget(self.button3, 2, 1, 1, 2)
+
+        # utilizasse window pq são exclusivos de window,
+        # widget não possui esses atributos:
+
+        # status bar
+        self.status_bar = self.statusBar()
+        self.status_bar.showMessage('a message')
+
+        # Slots -> Funções que se conectam a eventos:
+
+        # menu bar
+        self.menu_bar = self.menuBar()
+        self.first_menu = self.menu_bar.addMenu('first Menu')
+
+        # Signals -> Eventos:
+
+        self.first_action = self.first_menu.addAction('First action')
+        # trigger -> quando é clicado
+        # quando a minha ação for clicada, execute a função slot
+        self.first_action.triggered.connect(self.slot_example)
+
+        self.second_action = self.first_menu.addAction('Second action')
+        # quando a minha ação for clicada, check
+        self.second_action.setCheckable(True)
+        self.second_action.toggled.connect(self.second_slot_check)
+        # quando meu mouse estiver em cima:
+        self.second_action.hovered.connect(self.second_slot_check)
+
+    @Slot()
+    def slot_example(self, status_bar):
+        status_bar.showMessage('Show this Message')
+
+    @Slot()
+    def second_slot_check(self):
+        print('is checked?', self.second_action.isChecked())
+
+    def make_button(self, name: str):
+        button = QPushButton(name)
+        button.setStyleSheet('font-size: 35px; color: purple')
+        return button
 
 
-# utilizasse window pq são exclusivos de window,
-# widget não possui esses atributos:
+if __name__ == '__main__':
+    # a vida da minha tela:
+    app = QApplication(sys.argv)
 
-# status bar
-status_bar = window.statusBar()
-status_bar.showMessage('a message')
+    window = MyWindow()
+    window.show()
 
-# Slots -> Funções que se conectam a eventos:
-
-
-@Slot()
-def slot_example(status_bar):
-    status_bar.showMessage('Show this Message')
-
-
-@Slot()
-def other_slot(checked):
-    print('is checked?', checked)
-
-# adiando a execução do slot:
-# o signal hovered não possui argumentos a serem passados
-# então a função interna verifica se está checkado, passando assim o True/False
-# correspondente
-
-
-# def thirdy_slot(action):
-#     def inner():
-#         other_slot(action.isChecked())
-#     return inner
-
-# mesma coisa com lambda:
-@Slot()
-def thirdy_slot(action):
-    return other_slot(action.isChecked())
-
-
-# menu bar
-menu_bar = window.menuBar()
-first_menu = menu_bar.addMenu('first Menu')
-
-# Signals -> Eventos:
-
-first_action = first_menu.addAction('First action')
-# trigger -> quando é clicado
-# quando a minha ação for clicada, execute a função slot
-first_action.triggered.connect(lambda: slot_example(status_bar))
-
-second_action = first_menu.addAction('Second action')
-# quando a minha ação for clicada, check
-second_action.setCheckable(True)
-second_action.toggled.connect(other_slot)  # Me conectando a um slot
-# quando meu mouse estiver em cima:
-second_action.hovered.connect(lambda: thirdy_slot(second_action))
-
-button.clicked.connect(lambda: thirdy_slot(second_action))
-
-window.show()
-app.exec()  # loop da aplicação
+    app.exec()  # loop da aplicação
