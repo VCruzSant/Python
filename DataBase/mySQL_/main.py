c7fc31d348e1cc6154d667528f2217821c2b1c01
VCruzSant <sviniantiago@gmail.com>
2023-10-19 10:29:30 -0300

    Config Connection

---
+++ DataBase/mySQL_/main.py
@@ -0,0 +1,23 @@
+# PyMySQL - um cliente MySQL feito em Python Puro
+# Doc: https://pymysql.readthedocs.io/en/latest/
+# Pypy: https://pypi.org/project/pymysql/
+# GitHub: https://github.com/PyMySQL/PyMySQL
+import os
+import dotenv
+import pymysql
+
+dotenv.load_dotenv()
+
+con = pymysql.connect(
+    host=os.environ['MYSQL_HOST'],
+    user=os.environ['MYSQL_USER'],
+    password=os.environ['MYSQL_PASSWORD'],
+    database=os.environ['MYSQL_DATABASE']
+)
+cursor = con.cursor()
+
+# SQL:
+
+
+cursor.close()
+con.close()
