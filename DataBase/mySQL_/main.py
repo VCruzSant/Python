3e049ba57432277cefcf25b60f0fc092c40f9e06
VCruzSant <sviniantiago@gmail.com>
2023-10-19 11:34:22 -0300

    Insert Values with placeholders

--- DataBase/mySQL_/main.py
+++ DataBase/mySQL_/main.py
@@ -8,6 +8,8 @@ import pymysql
 
 dotenv.load_dotenv()
 
+TABLE_NAME = 'customers'
+
 con = pymysql.connect(
     host=os.environ['MYSQL_HOST'],
     user=os.environ['MYSQL_USER'],
@@ -17,6 +19,28 @@ con = pymysql.connect(
 cursor = con.cursor()
 
 # SQL:
+cursor.execute(
+    f'CREATE TABLE IF NOT EXISTS {TABLE_NAME} ('
+    'id INT NOT NULL AUTO_INCREMENT, '
+    'name VARCHAR(50) NOT NULL, '
+    'age INT NOT NULL, '
+    'PRIMARY KEY (id)'
+    ') '
+)
+# WARNING: esse comando limpa a tabela
+# Em ambiente de testes tudo bem, NUNCA fazer isso em produção
+cursor.execute(f'TRUNCATE TABLE {TABLE_NAME}')
+con.commit()
+
+sql = (
+    f'INSERT INTO {TABLE_NAME} '
+    '(name, age) '
+    'VALUES ("%s", %s) '
+)
+# Tupla -> não posso editar, Lista -> posso editar
+data = ('Sant', 20)
+cursor.execute(sql, data)
+con.commit()
 
 
 cursor.close()
